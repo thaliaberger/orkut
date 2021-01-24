406ba97fbcfc80f286fd48155f83695a7399e333
thaliaberger <thaliaberger@hotmail.com>
2021-01-24 14:00:10 -0300

    done

--- index.js
+++ index.js
@@ -6,15 +6,68 @@ const input = document.getElementById("input");
 const inputLink = document.getElementById("input-link");
 const depo = document.getElementById("profile-depo");
 
-const fotosPopup = document.getElementById("fotosPopup");
-const videosPopup = document.getElementById("videosPopup");
-const depoPopup = document.getElementById("depoPopup");
-
 const aboutFotos = document.getElementById("aboutFotos");
 const profileFotos = document.getElementById("profile-fotos");
 
 const aboutVideos = document.getElementById("aboutVideos");
 const profileVideos = document.getElementById("profile-videos");
+
+const fotosSeta = document.getElementById("fotos-seta");
+const videosSeta = document.getElementById("videos-seta");
+const deposSeta = document.getElementById("depos-seta");
+
+const fotosPopup = document.getElementById("fotosPopup");
+const videosPopup = document.getElementById("videosPopup");
+const depoPopup = document.getElementById("depoPopup");
+
+const verDepos = document.getElementById("ver-depos");
+const verVideos = document.getElementById("ver-videos");
+const verFotos = document.getElementById("ver-fotos");
+
+verDepos.onclick = function () {
+  depoPopup.setAttribute("class", "depo-popup-block");
+  setTimeout(function () {
+    depoPopup.setAttribute("class", "depo-popup");
+  }, 2000);
+};
+
+verFotos.onclick = function () {
+  fotosPopup.setAttribute("class", "fotos-popup-block");
+  setTimeout(function () {
+    fotosPopup.setAttribute("class", "fotos-popup");
+  }, 2000);
+};
+
+verVideos.onclick = function () {
+  videosPopup.setAttribute("class", "videos-popup-block");
+  setTimeout(function () {
+    videosPopup.setAttribute("class", "videos-popup");
+  }, 2000);
+};
+
+fotosSeta.onclick = function () {
+  if (fotosSeta.getAttribute("class") === "right") {
+    fotosSeta.setAttribute("class", "bottom");
+  } else {
+    fotosSeta.setAttribute("class", "right");
+  }
+};
+
+videosSeta.onclick = function () {
+  if (videosSeta.getAttribute("class") === "right") {
+    videosSeta.setAttribute("class", "bottom");
+  } else {
+    videosSeta.setAttribute("class", "right");
+  }
+};
+
+deposSeta.onclick = function () {
+  if (deposSeta.getAttribute("class") === "right") {
+    deposSeta.setAttribute("class", "bottom");
+  } else {
+    deposSeta.setAttribute("class", "right");
+  }
+};
 
 items.addEventListener("mouseover", function () {
   legenda.setAttribute("class", "legenda");
@@ -35,31 +88,23 @@ fas.onclick = function () {
 };
 
 aboutFotos.onclick = function () {
-  fotosPopup.setAttribute("class", "fotos-popup-block");
-  setTimeout(function () {
-    fotosPopup.setAttribute("class", "fotos-popup");
-  }, 2000);
+  window.scrollTo({ top: 1000, behavior: "smooth" });
 };
 
 aboutVideos.onclick = function () {
-  videosPopup.setAttribute("class", "videos-popup-block");
-  setTimeout(function () {
-    videosPopup.setAttribute("class", "videos-popup");
-  }, 2000);
+  window.scrollTo({ top: 1000, behavior: "smooth" });
 };
 
 profileFotos.onclick = function () {
-  fotosPopup.setAttribute("class", "fotos-popup-block");
-  setTimeout(function () {
-    fotosPopup.setAttribute("class", "fotos-popup");
-  }, 2000);
+  window.scrollTo({ top: 1000, behavior: "smooth" });
 };
 
 profileVideos.onclick = function () {
-  videosPopup.setAttribute("class", "videos-popup-block");
-  setTimeout(function () {
-    videosPopup.setAttribute("class", "videos-popup");
-  }, 2000);
+  window.scrollTo({ top: 1000, behavior: "smooth" });
+};
+
+depo.onclick = function () {
+  window.scrollTo({ top: 1000, behavior: "smooth" });
 };
 
 input.onchange = function () {
@@ -67,13 +112,6 @@ input.onchange = function () {
     "href",
     `https://www.google.com/search?q=${input.value}`
   );
-};
-
-depo.onclick = function () {
-  depoPopup.setAttribute("class", "depo-popup-block");
-  setTimeout(function () {
-    depoPopup.setAttribute("class", "depo-popup");
-  }, 2000);
 };
 
 window.onload = () => {
